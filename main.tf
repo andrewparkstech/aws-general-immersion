10bd4bbd5cfacc7aab7222d6286e0329d0fbb101
Andrew Parks <aparks@gmail.com>
2024-07-03 23:48:08 -0400

    added role for Session Manager

--- main.tf
+++ main.tf
@@ -56,8 +56,40 @@ resource "aws_instance" "web_server" {
   key_name = "AWS-ImmersionDay"
   security_groups = [aws_security_group.web_server_sg.name]
   user_data = file("${path.module}/userdata.sh")
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
   
   tags = {
     Function = "AWS-Immersion"
   }
+}
+
+resource "aws_iam_role" "ssm_instance_role" {
+  name = "SSMInstanceProfile"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ssm_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "SSMInstanceProfile"
+  role = aws_iam_role.ssm_instance_role.name
+}
+
+output "ssm_instance_profile_arn" {
+  value = aws_iam_instance_profile.ssm_instance_profile.arn
 }
