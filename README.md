167cfb5e3ac6ddeb30911d7d68d5f52d933b20cf
Andrew Parks <aparks@gmail.com>
2024-07-09 21:46:16 -0400

    Added documentation

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# aws-general-immersion
+# AWS General Immersion Day
 
-Lab for AWS General Immersion Day, done in Terraform
+Labs for AWS General Immersion Day, done in Terraform
 
-[AWS General Immersion Day](https://catalog.workshops.aws/general-immersionday/en-US)
+Link: [AWS General Immersion Day](https://catalog.workshops.aws/general-immersionday/en-US)
+
+### Intro
+There are various labs over at AWS General Immersion Day. The one I completed was for EC2 Auto scaling. It walks you through setting it up using the console (GUI) but I decided to use Terraform because:
+* I like a challenge
+* It's fun
+
+### Authentication
+Authentication to AWS for this lab is done via credentials supplied to the AWS command line (local credentials file). Best practice for production is to use AWS Identity Center.
+
--- main.tf
+++ main.tf
@@ -37,52 +37,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# resource "aws_security_group" "web_server_sg" {
-#   name        = "Immersion Day Web Server"
-#   description = "Security group for web server allowing SSH and HTTP from anywhere"
-#   vpc_id      = data.aws_vpc.default.id
-
-#   ingress {
-#     description = "Allow SSH"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     description = "Allow HTTP"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1" # Allows all outbound traffic
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Function = "AWS-ImmersionDay"
-#   }
-# }
-
-# resource "aws_instance" "web_server" {
-#   ami           = "ami-06c68f701d8090592" # Amazon Linux 2023 64-bit (x86)
-#   instance_type = "t2.micro"
-#   key_name = "AWS-ImmersionDay"
-#   security_groups = [aws_security_group.web_server_sg.name]
-#   user_data = file("${path.module}/userdata.sh")
-#   iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
-  
-#   tags = {
-#     Function = "AWS-ImmersionDay"
-#   }
-# }
-
 resource "aws_security_group" "Auto_Scaling_SG" {
   name        = "Auto Scaling SG"
   description = "Security group for ASG"
